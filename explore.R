67d48db85ed3075c4560c0e1dbe991c86d538406
ryandkerr <ryankerr@college.harvard.edu>
2015-06-09 23:24:03 -0400

    added exploratory r script, data file not included due to size

---
+++ explore.R
@@ -0,0 +1,15 @@
+# Ryan Kerr
+# explore-yelp.R
+# exploring the academic yelp! dataset
+
+library(jsonlite)
+
+yelp_base <- stream_in(file("data/yelp_academic_dataset.json"))
+
+biz <- yelp_base[yelp_base$type == "business",]
+
+# harvard_mit <- biz[biz$state == "MA",]
+
+harvard = biz[biz$neighborhoods == "Harvard Square",]
+
+
